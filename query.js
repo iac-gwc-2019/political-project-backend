0a1ef261df4a51d46b5dc8696c414e0d5097dd4d
nzadia <nadiazhossain@gmail.com>
2019-07-17 14:33:16 -0400

    created query.js and added a line to schemas.sj

---
+++ query.js
@@ -0,0 +1,13 @@
+async function feed(parent, args, context, info) {
+  const where = args.filter ? {
+    OR: [
+      { description_contains: args.filter },
+      { url_contains: args.filter },
+    ],
+  } : {}
+
+  const links = await context.prisma.links({
+    where
+  })
+  return links
+}
